168627b8d3b3a93d280bd7de218ce730ca9c8447
Amin HIjaz <aminhijaz2001@gmail.com>
2020-06-27 12:02:01 +0300

    Hello code for palestine

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={require('./logo.png')} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          What is water polo
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render() {
+    const group = "Code for Palestine"
+    return (
+    <h1>Hello {group}</h1>
+  
+    );
+  }
+ 
 }
-
 export default App;
